368bae2f0ebae2cc580cc31b738e0ce7bf9dc8a7
DudkaP <broxik666@gmail.com>
2022-05-25 15:42:12 +0200

    fix

--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -4,5 +4,7 @@ import {environment} from "../../environments/environment";
 const {API} = environment;
 
 export const urls = {
-
+  auth: `${API}/auth`,
+  users: `${API}/users`,
+  cars: `${API}/cars`
 }
